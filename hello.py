5e3af14cbccf333ba21d12268ee3f20e48d2b64b
Adam Cox <adamcox.dev@gmail.com>
2022-10-10 19:39:30 -0600

    played around with Tuples and Sets

--- hello.py
+++ hello.py
@@ -9,3 +9,23 @@ print(f'Hello, his name is {name}. His age is {age}.')
 d = {'k': 2222, 'k1': 4.99, 'k3': 13.99}
 
 print(d['k3'])
+
+d['k4'] = 'Yes Brah'
+
+print(d)
+
+t = (1,2,3)
+
+newList = [1,2,3]
+
+newList[0] = 'Yes'
+
+print(newList)
+
+myset = set()
+
+myset.add(1)
+
+myset.add(2)
+
+print(myset)
