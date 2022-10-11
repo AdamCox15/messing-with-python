e206a3d1e0bfb75d35c95264078836adab3b4112
Adam Cox <adamcox.dev@gmail.com>
2022-10-10 19:24:00 -0600

    First commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("Hello from python")
+print(1000 + 300)
+
+name = "Adam"
+age ="32"
+
+print(f'Hello, his name is {name}. His age is {age}.')
+
+d = {'k': 2222, 'k1': 4.99, 'k3': 13.99}
+
+print(d['k3'])
